af01f9505f712b370ee78a36c907ba6910ac22ff
Lim0811 <bikrambora0811@gmail.com>
2025-07-27 22:47:08 +0530

    update

--- 04.c
+++ 04.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int n, i;
-    int t1 = 0, t2 = 1, nextTerm;
+int main() 
+{
+    int n, first = 0, second = 1, next;
 
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    if (n <= 0) {
-        printf("Please enter a positive integer.\n");
-        return 1;
-    }
-
     printf("Fibonacci Series: ");
 
-    for (i = 1; i <= n; ++i) {
-        if (i == 1) {
-            printf("%d ", t1);
-        } else if (i == 2) {
-            printf("%d ", t2);
-        } else {
-            nextTerm = t1 + t2;
-            t1 = t2;
-            t2 = nextTerm;
-            printf("%d ", nextTerm);
-        }
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
     }
-    printf("\n");
 
     return 0;
 }
